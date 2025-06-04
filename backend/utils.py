c410803506fea9a4ed778634f738b72ecf36e60e
Fareed Hayat <110565459+fareedhayat@users.noreply.github.com>
2025-06-04 20:55:09 +0500

    Delete backend directory

--- backend/utils.py
+++
@@ -1,9 +0,0 @@
-_collected_answers: dict[str,str] = {}
-
-def input_once(prompt: str, input_func=input) -> str:
-    key = prompt.strip().lower()
-    if key in _collected_answers:
-        return _collected_answers[key]
-    ans = input_func(prompt)
-    _collected_answers[key] = ans
-    return ans
